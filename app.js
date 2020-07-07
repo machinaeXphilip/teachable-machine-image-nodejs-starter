67ede9bbfafa4e06c1549e15e98a8f80ade1c624
machinaeXphilip <gauguerilla@gmail.com>
2020-07-07 11:02:39 +0200

    added my own model. took notes for fork edits to build up microservice on top

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+/* 
+micro service to predict the content of a picture
+using models trained with https://teachablemachine.withgoogle.com/
+
+will be used in the interactive telegram theater performance by https://www.machinaex.com
+
+build from the starter code from https://github.com/traumverloren/teachable-machine-image-nodejs-starter 
+a boilerplate/starter that does not require to edit the teachablemachine lib :rocket
+
+explanation: without traumverloren's starter code (s.a.) one had to modify node_modules/@teachablemachine/image/dist/utils/canvas.js to exclude HTMLVideoElement (TODO: PULL REQUEST or issue!!!!)
+and emulate the HTML DOM and document and canvas elements within node which her code does within the predict.js module. The proof of concept for that I found at:
+https://github.com/tr7zw/teachablemachine-node-example. traumverloren's starter code i found in this issue: 
+https://github.com/googlecreativelab/teachablemachine-community/issues/33#issuecomment-612620670
+
+*/
+
 const tf = require('@tensorflow/tfjs-node');
 const path = require('path');
 const { loadImage } = require('canvas');
@@ -11,7 +27,7 @@ let model;
 (async function main() {
   try {
     // Load your image
-    const image = await loadImage(path.resolve(__dirname, './YOUR_IMAGE.jpg'));
+    const image = await loadImage(path.resolve(__dirname, './cup.jpg'));
 
     // Load the trained Teachable Machine model
     model = await tf.loadLayersModel(DEFAULT_MODEL_LOCATION);
@@ -23,6 +39,7 @@ let model;
     // Get the top result's name
     const topResult = results[0].className;
     console.log('Prediction:', topResult);
+    console.log('Predictions:', results);
   } catch (e) {
     console.error(e);
   }
